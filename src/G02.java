a82af964b7bff4d1c36159ea8fbff5e59c235a70
trtkyunus <tartikyunus656@gmail.com>
2023-06-19 09:39:29 +0200

    4.gün

---
+++ src/G02.java
@@ -0,0 +1,21 @@
+public class G02 {
+
+    /*
+    Git-Github'da projelerimizin uzerinde calisirken temelde 3 adimda islem yapiyoruz.
+
+       1- Projelerimizi/Dosyalarimizi git ile iliskilendiriyoruz.
+          git init
+       2- Projemizde/dosyamizda bulunanlari versiyon olusturuyorduk.
+
+          git add .
+          git commit -m "versiyonadi"
+
+        3- github'a yollama/entegrasyonu
+
+           git remote add origin repoUrlAdresi
+           already origin
+           git remote set-url repoUrlAdresi
+
+           git push
+     */
+}
